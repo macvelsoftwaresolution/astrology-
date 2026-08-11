279dd21e3d32e282e25396fe937e9d4fc516db7a
Logesh <24pca150@anjaconline.org>
2026-08-11 11:41:00 +0530

    add landing page

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mobile',
+  appId: 'com.aruthra.astrology',
+  appName: 'Aruthra Astrology',
   webDir: 'www'
 };
 
