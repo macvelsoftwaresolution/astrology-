bbb3f1efba72c7b5709bed537b5f16e7903abf46
Logesh <24pca150@anjaconline.org>
2026-09-01 15:49:30 +0530

    env added

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { Capacitor } from '@capacitor/core';
 
 export const environment = {
-  apiUrl: 'https://sriaarudhraaastro.com/api',
+  apiUrl: 'https://sriaarudhraaastro.com/api', // For local dev, change to: 'http://localhost:8000/api'
   pathUrl: 'https://sriaarudhraaastro.com',
   production: false
 };
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://sriaarudhraaastro.com/api',
+  apiUrl: 'https://sriaarudhraaastro.com/api', // For local dev, change to: 'http://localhost:8000/api'
   pathUrl: 'https://sriaarudhraaastro.com',
 };
